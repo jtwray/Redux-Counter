dacb2b6387a93de9de523714804d615738d46b19
jtwray <tuckerwray84@gmail.com>
2019-07-12 21:25:28 -0400

    Creates a reducer function for actions increment and decrement. assigns initialState{count:0} pure functions with switch statement cases. uses spread operator to create new version of state/immutable

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,23 @@
-import { INCREMENT, DECREMENT } from '../actions';
+import {INCREMENT, DECREMENT} from "../actions";
 
-const initialState = {
-  count: 0
+const initialState={count:0};
+
+const  reducer=( state=initialState, action ) => {
+	switch( action.type ) {
+		case INCREMENT:
+			return {
+				...state,
+				count: state.count+action.payload
+			}
+		// Fill in the body of this case
+		case DECREMENT:
+			return {
+				...state,
+				count: state.count-action.payload
+			}
+		default:
+			return state;
+	}
 }
 
-// Our reducer that handles our two action cases:
-// increment and decrement. It receives the state
-// of our redux store, along with an action created
-// by our action creator. What does the reducer
-// need to do with the count in each case?
-export default (state = initialState, action) => {
-  switch (action.type) {
-    case INCREMENT:
-    // Fill in the body of this case
-    case DECREMENT:
-    // Fill in the body of this case
-    default:
-      return state;
-  }
-};
+export default reducer
